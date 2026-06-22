ac3178e8931b1093ba2107d8560e2a9e4bb740f4
Hector Dickens <hache-manuel28@hotmail.com>
2026-06-21 21:25:42 -0500

    tes

--- quiniela-frontend/src/App.jsx
+++ quiniela-frontend/src/App.jsx
@@ -11,12 +11,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBfl4KjCjacixbErImt8PkI72GpI2J_1EU",
+  authDomain: "quiniela-worldcup-43262.firebaseapp.com",
+  projectId: "quiniela-worldcup-43262",
+  storageBucket: "quiniela-worldcup-43262.appspot.com",
+  messagingSenderId: "25784077559",
+  appId: "1:25784077559:web:b9eb933e906725929517f2"
 };
 
 const app = initializeApp(firebaseConfig);
